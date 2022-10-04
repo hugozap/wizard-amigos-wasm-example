13c74ffd3cdc2133747ae4efb2905872e667f563
Hugo Zapata <hugozap@gmail.com>
2022-10-04 00:26:03 -0500

    updated, image manipulation example using photon Rust crate

--- webworker.js
+++ webworker.js
@@ -6,12 +6,12 @@ importScripts("./rust_wasm_js_interop.js");
 (async function () {
   let wasm = fetch("/rust_wasm_js_interop_bg.wasm");
   await wasm_bindgen(wasm);
-  const {greet} = wasm_bindgen;
+  const {factorial} = wasm_bindgen;
 
   // listen for messages from the main thread
   self.addEventListener("message", (event) => {
-    console.log("Message received from main thread");
-    const result = greet  (event.data);
+    console.log("Message received from main thread"+event.data);
+    const result = factorial(event.data);
     postMessage(result);
   });
 })();
