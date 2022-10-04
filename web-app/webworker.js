d7320218440a324e92d8b6e2d6da89eb84e55e1f
Hugo Zapata <hugozap@gmail.com>
2022-10-04 13:26:56 -0500

    update

--- web-app/webworker.js
+++ web-app/webworker.js
@@ -6,11 +6,11 @@ importScripts("./rust_wasm_js_interop.js");
 (async function () {
   let wasm = fetch("/rust_wasm_js_interop_bg.wasm");
   await wasm_bindgen(wasm);
-  const {factorial} = wasm_bindgen;
+  const { factorial } = wasm_bindgen;
 
   // listen for messages from the main thread
   self.addEventListener("message", (event) => {
-    console.log("Message received from main thread"+event.data);
+    console.log("Message received from main thread" + event.data);
     const result = factorial(event.data);
     postMessage(result);
   });
